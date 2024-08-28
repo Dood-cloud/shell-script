3aa41b4ef4f9ce71a476f001302d54db83a88c4f
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 23:02:04 +0530

    reading input frm user

---
+++ 06-variablesfromuser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "What is your Username??"
+
+read -s USERNAME
+
+echo "Your username is $USERNAME"
+
+echo "What is your Password??"
+
+read PASSWORD
+
+echo "Your password is $PASSWORD"
