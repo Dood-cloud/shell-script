f19f4d60b304a368104c13494328a26a388af0ff
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 22:17:52 +0530

    conversation

---
+++ 03-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Naveen : Hi Ravi there is one record which should be completed today. will you be able to do it??"
+echo "Ravindra : Hi Naveen, today APAC is very busy and got so many records. may not be able to take it."
+echo "Naveen : understand. But today we are expecting huge volumes in EMEA and NAM. Sooo....."
+echo "Ravindra : Did you ask Raje about this record?"
+echo "Naveen : Yes, but it seems she has records with more no.of PEPs. she cannot take it"
+echo "Ravindra : Ok then Naveen assign it to me. I will try to complete it asap."
+echo "Naveen : Thank you Ravindra. You are always and had been the saviour of last resort"
