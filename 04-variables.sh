e7750cdef44c669c3e20fe8c47b4a236e66ffd1f
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 22:33:41 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,9 +2,10 @@
 
 NAME1=Anisha
 NAME2=Subba
+REGION=EMEA
 
 echo "$NAME1 : Hi $NAME2 there is one record which should be completed today. will you be able to do it??"
-echo "$NAME2 : Hi $NAME1, today APAC is very busy and got so many records. may not be able to take it."
+echo "$NAME2 : Hi $NAME1, today $REGION is very busy and got so many records. may not be able to take it."
 echo "$NAME1 : understand. But today we are expecting huge volumes in EMEA and NAM. Sooo....."
 echo "$NAME2 : Did you ask Raje about this record?"
 echo "$NAME1 : Yes, but it seems she has records with more no.of PEPs. she cannot take it"
