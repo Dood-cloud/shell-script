b70cee25ce2a588ab9c89886ff6b989c8887a36b
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 23:14:18 +0530

    user input

--- 06-variablesfromuser.sh
+++ 06-variablesfromuser.sh
@@ -8,6 +8,18 @@ echo "Your username is $USERNAME"
 
 echo "What is your Password??"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "Your password is $PASSWORD"
+
+echo "What is your Username??"
+
+read -s USERNAME2
+
+echo "Your username is $USERNAME2"
+
+echo "What is your Password??"
+
+read -s PASSWORD2
+
+echo "Your password is: hey how can you ask passwords??"
