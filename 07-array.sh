6a86fa70e6345fd56c401244d0f6c62f980a1db9
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 23:34:29 +0530

    array

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=(“STRAWBERRY” “MANGO” “BANANA”)
+
+echo “Name of first fruit is: $FRUITS[0]”
+
+echo “Name of second fruit is: $FRUITS[1]”
+
+echo “Name of third fruit is: ${FRUITS[2]}”
+
+echo “Name of all fruits in the basket are: $FRUITS[@]”
