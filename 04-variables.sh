457ac362e80d2985e73e1a60293261a38ae2f9c3
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 22:25:20 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+1NAME=Anisha
+2NAME=Subba
+
+echo "$1NAME : Hi $2NAME there is one record which should be completed today. will you be able to do it??"
+echo "$2NAME : Hi $1NAME, today APAC is very busy and got so many records. may not be able to take it."
+echo "$1NAME : understand. But today we are expecting huge volumes in EMEA and NAM. Sooo....."
+echo "$2NAME : Did you ask Raje about this record?"
+echo "$1NAME : Yes, but it seems she has records with more no.of PEPs. she cannot take it"
+echo "$2NAME : Ok then Naveen assign it to me. I will try to complete it asap."
+echo "$1NAME : Thank you $2NAME. You are always and had been the saviour of last resort"
