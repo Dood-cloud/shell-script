cd9ab0574b1a33d4c2ca14adf0819bb71169b4e3
Ravindra Doodala <doodalar@gmail.com>
2024-08-28 22:40:17 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME1=Naveen
+NAME2=Abdul
+REGION=NAM
+
+$NAME1 : Hi $NAME2 there is one record which should be completed today. will you be able to do it??
+$NAME2 : Hi $NAME1, today $REGION is very busy and got so many records. may not be able to take it.
+$NAME1 : understand. But today we are expecting huge volumes in EMEA and NAM. Sooo.....
+$NAME2 : Did you ask Raje about this record?
+$NAME1 : Yes, but it seems she has records with more no.of PEPs. she cannot take it
+$NAME2 : Ok then $NAME1 assign it to me. I will try to complete it asap.
+$NAME1: Thank you $NAME2. You are always and had been the savior of last resort
